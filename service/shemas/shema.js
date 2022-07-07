823db10cb78feee913f2d35328b8a94baa5b3599
codajoker <84983434+codajoker@users.noreply.github.com>
2022-07-07 22:10:17 +0200

    added basic schema for user registration

---
+++ service/shemas/shema.js
@@ -0,0 +1,48 @@
+const Joi = require("joi");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const users = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+  
+    token: {
+      type: String,
+      default: null,
+    },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+        type: String,
+        default: null,
+        },
+    
+  },
+  {
+    versionKey: false,
+  }
+);
+const joiSchema = Joi.object({
+  password: Joi.string().min(6).max(30).required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .required(),
+  token: Joi.string(),
+});
+
+const User = mongoose.model("user", users);
+
+module.exports = { User, joiSchema,  };
