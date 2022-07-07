c92de35bf71cf0e32bb9bc44ff312f3cc1992881
codajoker <84983434+codajoker@users.noreply.github.com>
2022-07-07 11:33:15 +0200

    based

--- app.js
+++ app.js
@@ -5,24 +5,15 @@ const cool = require('cool-ascii-faces');
 
 const app = express();
 
-// const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-// app.use(logger(formatsLogger));
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-//   app.get('/', (req, res) => res.send(cool()))
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+  app.get('/', (req, res) => res.send(cool()))
 
-//   app.get('/cool', (req, res) => res.send(cool()))
-
-
-const path = require('path')
-
-app
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  app.get('/cool', (req, res) => res.send(cool()))
 
 
 module.exports = app;
