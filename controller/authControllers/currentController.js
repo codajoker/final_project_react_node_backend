571689d2d4a03cff0eb1235840d7ceb38327b911
codajoker <84983434+codajoker@users.noreply.github.com>
2022-07-14 17:29:43 +0200

    fix current UserController and add swagger documentation

--- controller/authControllers/currentController.js
+++ controller/authControllers/currentController.js
@@ -3,12 +3,7 @@ const { User } = require("../../service/shemas/shema");
 
 const currentController = async (req, res,) => {
     const { token } = req.user; 
-    if (!token) {
-      return res.status(401).json({
-        status: "Error",
-        message: "User not found",
-      });
-    }
+   
     const user = await User.findOne({ token });
     
   res.status(200).json({
