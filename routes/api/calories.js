7bb040eb316915d59b4bb7bcb22f1536107230a0
codajoker <84983434+codajoker@users.noreply.github.com>
2022-07-15 18:53:52 +0200

    Update calories.js

--- routes/api/calories.js
+++ routes/api/calories.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const { validationAuth } = require("../../service/validations");
 const { JoiSchemaCalories } = require("../../service/shemas/shema");
 const { ctrlWrapper } = require("../../middleware/ctrlWrapper");
-const { caloriesController } = require("../../controller/caloriesControllers");
+const { caloriesController } = require("../../controller/caloriesControllers/caloriesController");
 
 const { auth } = require("../../middleware/auth");
 // create public endpoin for calories and products
